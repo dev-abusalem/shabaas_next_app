771f964bdfa9f92b984638581c89440a65382756
dev.abusalem <dev.abusalem@gmail.com>
2023-09-23 19:17:42 +0600

    update some

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,9 @@
-import '@/styles/globals.css'
-import { Provider } from 'react-redux';
-import 'react-toastify/dist/ReactToastify.css';
-import store from '@/redux/store/store';
+import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Provider store={store}>
       <Component {...pageProps} />
-    </Provider>
     </>
-  )
+  );
 }
- 
