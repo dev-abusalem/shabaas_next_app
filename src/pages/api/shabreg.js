48d7cb4d35b9f2aabc13c4f0a3f0a2b8c09880e8
dev.abusalem <dev.abusalem@gmail.com>
2023-09-24 01:59:44 +0600

    update reg page

--- src/pages/api/shabreg.js
+++ src/pages/api/shabreg.js
@@ -1,28 +1,25 @@
-import connectDB from "../db/connectDB";
-import User2 from '../../models/User'; // Adjust the path as needed
+import connectDB from "../../server/db/connectDB";
+import User2 from "../../server/models/User"; // Adjust the path as needed
 
 export default async function handler(req, res) {
+  await connectDB(); // Call the connectDB function to establish the database connection
 
-    await connectDB(); // Call the connectDB function to establish the database connection
+  if (req.method === "POST") {
+    try {
+      // Example usage: Create a new user
+      const newUser = new User2({
+        email: req.body.email,
+        password: req.body.password,
+      });
 
-    if (req.method === 'POST') {
-            try {
-                // Example usage: Create a new user
-                const newUser = new User2({
-                    email: req.body.email,
-                    password: req.body.password,
-                });
-        
-                await newUser.save();
-        
-                res.status(200).json({ message: 'User created successfully' });
-            } catch (error) {
-                console.error('Error creating user:', error);
-                res.status(500).json({ error: 'Server error' });
-            }
-        
-    } else {
-        res.status(500).json("Method Not Allowed");
+      await newUser.save();
+
+      res.status(200).json({ message: "User created successfully" });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).json({ error: "Server error" });
     }
-    
+  } else {
+    res.status(500).json("Method Not Allowed");
+  }
 }
